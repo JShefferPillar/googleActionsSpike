7e2a2534a9a723afedadffcda80a7bba8aee9196
Yasmine Minachi <yasi@Yasmines-MacBook-Pro.local>
2018-11-28 13:08:08 -0500

    auto-generating uuids

--- dialogFlow/intents/reportOutageFlow/collectAddressIntent.js
+++ dialogFlow/intents/reportOutageFlow/collectAddressIntent.js
@@ -1,113 +1,7 @@
 const {outageContext, addressGivenContext} = require('../../contexts/outageContexts.js');
 const finalConfirmIntent = require('./finalConfirmIntent');
 const repeatAddressIntent = require('./repeatAddressIntent');
-
-const trainingText = [
-    "yeah",
-    "yes",
-    "ok",
-    "correct",
-    "sure",
-    "okay",
-    "sounds correct",
-    "that works",
-    "yep that\u0027s ok",
-    "yes that\u0027s alright",
-    "I think so",
-    "sg",
-    "yes I agree",
-    "I don\u0027t mind",
-    "I agree",
-    "yes I do",
-    "for sure",
-    "ok",
-    "yes that\u0027t ok",
-    "that one works",
-    "yes you can do it",
-    "perfect",
-    "yes",
-    "why not",
-    "of course",
-    "yep that\u0027s right",
-    "okay I will",
-    "exactly",
-    "sure why not",
-    "absolutely",
-    "it\u0027s okay",
-    "it\u0027s fine",
-    "go ahead",
-    "confirm",
-    "sounds good",
-    "alright",
-    "yeah",
-    "yup",
-    "yes please",
-    "do it",
-    "yes I can",
-    "it looks perfect",
-    "that\u0027s correct",
-    "right",
-    "alright why not",
-    "yep"
-];
-
-const uuids = [
-    "2caf7bfa-1506-4279-a921-38120c19e01",
-    "2caf7bfa-1506-4279-a921-38120c19e02",
-    "2caf7bfa-1506-4279-a921-38120c19e03",
-    "2caf7bfa-1506-4279-a921-38120c19e04",
-    "2caf7bfa-1506-4279-a921-38120c19e05",
-    "2caf7bfa-1506-4279-a921-38120c19e06",
-    "2caf7bfa-1506-4279-a921-38120c19e07",
-    "2caf7bfa-1506-4279-a921-38120c19e08",
-    "2caf7bfa-1506-4279-a921-38120c19e09",
-    "2caf7bfa-1506-4279-a921-38120c19e10",
-    "2caf7bfa-1506-4279-a921-38120c19e11",
-    "2caf7bfa-1506-4279-a921-38120c19e12",
-    "2caf7bfa-1506-4279-a921-38120c19e13",
-    "2caf7bfa-1506-4279-a921-38120c19e14",
-    "2caf7bfa-1506-4279-a921-38120c19e15",
-    "2caf7bfa-1506-4279-a921-38120c19e16",
-    "2caf7bfa-1506-4279-a921-38120c19e17",
-    "2caf7bfa-1506-4279-a921-38120c19e18",
-    "2caf7bfa-1506-4279-a921-38120c19e19",
-    "2caf7bfa-1506-4279-a921-38120c19e20",
-    "2caf7bfa-1506-4279-a921-38120c19e21",
-    "2caf7bfa-1506-4279-a921-38120c19e22",
-    "2caf7bfa-1506-4279-a921-38120c19e23",
-    "2caf7bfa-1506-4279-a921-38120c19e24",
-    "2caf7bfa-1506-4279-a921-38120c19e25",
-    "2caf7bfa-1506-4279-a921-38120c19e26",
-    "2caf7bfa-1506-4279-a921-38120c19e27",
-    "2caf7bfa-1506-4279-a921-38120c19e28",
-    "2caf7bfa-1506-4279-a921-38120c19e29",
-    "2caf7bfa-1506-4279-a921-38120c19e30",
-    "2caf7bfa-1506-4279-a921-38120c19e31",
-    "2caf7bfa-1506-4279-a921-38120c19e32",
-    "2caf7bfa-1506-4279-a921-38120c19e33",
-    "2caf7bfa-1506-4279-a921-38120c19e34",
-    "2caf7bfa-1506-4279-a921-38120c19e35",
-    "2caf7bfa-1506-4279-a921-38120c19e36",
-    "2caf7bfa-1506-4279-a921-38120c19e37",
-    "2caf7bfa-1506-4279-a921-38120c19e38",
-    "2caf7bfa-1506-4279-a921-38120c19e39",
-    "2caf7bfa-1506-4279-a921-38120c19e40",
-    "2caf7bfa-1506-4279-a921-38120c19e41",
-    "2caf7bfa-1506-4279-a921-38120c19e42",
-    "2caf7bfa-1506-4279-a921-38120c19e43",
-    "2caf7bfa-1506-4279-a921-38120c19e44",
-    "2caf7bfa-1506-4279-a921-38120c19e45"
-];
-
-const trainingPhrases = trainingText.map( (text, index) => {
-    return {
-        name: uuids[index],
-        type: "TEMPLATE",
-        parts: [{
-            text
-        }]
-    }
-});
+const trainingPhrases = require('../../trainingPhrases.js');
 
 const addressParameter = {
     name: "2c8d5d2f-50b7-4999-b1bb-75d9ebc8e83b",
@@ -131,7 +25,7 @@ const message = {
 
 const intent = {
     displayName: "collect_address",
-    trainingPhrases: trainingPhrases,
+    trainingPhrases: trainingPhrases.yes,
     messages: [message],
     parameters: [addressParameter],
     inputContextNames: [outageContext.name],
--- dialogFlow/intents/reportOutageFlow/finalConfirmIntent.js
+++ dialogFlow/intents/reportOutageFlow/finalConfirmIntent.js
@@ -1,112 +1,5 @@
 const {addressGivenContext} = require('../../contexts/outageContexts.js');
-
-const trainingText = [
-    "yeah",
-    "yes",
-    "ok",
-    "correct",
-    "sure",
-    "okay",
-    "sounds correct",
-    "that works",
-    "yep that\u0027s ok",
-    "yes that\u0027s alright",
-    "I think so",
-    "sg",
-    "yes I agree",
-    "I don\u0027t mind",
-    "I agree",
-    "yes I do",
-    "for sure",
-    "ok",
-    "yes that\u0027t ok",
-    "that one works",
-    "yes you can do it",
-    "perfect",
-    "yes",
-    "why not",
-    "of course",
-    "yep that\u0027s right",
-    "okay I will",
-    "exactly",
-    "sure why not",
-    "absolutely",
-    "it\u0027s okay",
-    "it\u0027s fine",
-    "go ahead",
-    "confirm",
-    "sounds good",
-    "alright",
-    "yeah",
-    "yup",
-    "yes please",
-    "do it",
-    "yes I can",
-    "it looks perfect",
-    "that\u0027s correct",
-    "right",
-    "alright why not",
-    "yep"
-];
-
-const uuids = [
-    "d34f7bfa-1506-4279-a921-38120c19e01",
-    "d34f7bfa-1506-4279-a921-38120c19e02",
-    "d34f7bfa-1506-4279-a921-38120c19e03",
-    "d34f7bfa-1506-4279-a921-38120c19e04",
-    "d34f7bfa-1506-4279-a921-38120c19e05",
-    "d34f7bfa-1506-4279-a921-38120c19e06",
-    "d34f7bfa-1506-4279-a921-38120c19e07",
-    "d34f7bfa-1506-4279-a921-38120c19e08",
-    "d34f7bfa-1506-4279-a921-38120c19e09",
-    "d34f7bfa-1506-4279-a921-38120c19e10",
-    "d34f7bfa-1506-4279-a921-38120c19e11",
-    "d34f7bfa-1506-4279-a921-38120c19e12",
-    "d34f7bfa-1506-4279-a921-38120c19e13",
-    "d34f7bfa-1506-4279-a921-38120c19e14",
-    "d34f7bfa-1506-4279-a921-38120c19e15",
-    "d34f7bfa-1506-4279-a921-38120c19e16",
-    "d34f7bfa-1506-4279-a921-38120c19e17",
-    "d34f7bfa-1506-4279-a921-38120c19e18",
-    "d34f7bfa-1506-4279-a921-38120c19e19",
-    "d34f7bfa-1506-4279-a921-38120c19e20",
-    "d34f7bfa-1506-4279-a921-38120c19e21",
-    "d34f7bfa-1506-4279-a921-38120c19e22",
-    "d34f7bfa-1506-4279-a921-38120c19e23",
-    "d34f7bfa-1506-4279-a921-38120c19e24",
-    "d34f7bfa-1506-4279-a921-38120c19e25",
-    "d34f7bfa-1506-4279-a921-38120c19e26",
-    "d34f7bfa-1506-4279-a921-38120c19e27",
-    "d34f7bfa-1506-4279-a921-38120c19e28",
-    "d34f7bfa-1506-4279-a921-38120c19e29",
-    "d34f7bfa-1506-4279-a921-38120c19e30",
-    "d34f7bfa-1506-4279-a921-38120c19e31",
-    "d34f7bfa-1506-4279-a921-38120c19e32",
-    "d34f7bfa-1506-4279-a921-38120c19e33",
-    "d34f7bfa-1506-4279-a921-38120c19e34",
-    "d34f7bfa-1506-4279-a921-38120c19e35",
-    "d34f7bfa-1506-4279-a921-38120c19e36",
-    "d34f7bfa-1506-4279-a921-38120c19e37",
-    "d34f7bfa-1506-4279-a921-38120c19e38",
-    "d34f7bfa-1506-4279-a921-38120c19e39",
-    "d34f7bfa-1506-4279-a921-38120c19e40",
-    "d34f7bfa-1506-4279-a921-38120c19e41",
-    "d34f7bfa-1506-4279-a921-38120c19e42",
-    "d34f7bfa-1506-4279-a921-38120c19e43",
-    "d34f7bfa-1506-4279-a921-38120c19e44",
-    "d34f7bfa-1506-4279-a921-38120c19e45"
-
-];
-
-const trainingPhrases = trainingText.map( (text, index) => {
-    return {
-        name: uuids[index],
-        type: "TEMPLATE",
-        parts: [{
-            text
-        }]
-    }
-});
+const trainingPhrases = require('../../trainingPhrases.js');
 
 const addressParameter = {
     name: "fbdd3636-dcd4-449e-933e-9d61008f3556",
@@ -117,7 +10,7 @@ const addressParameter = {
 
 const intent = {
     displayName: "final_confirm",
-    trainingPhrases: trainingPhrases,
+    trainingPhrases: trainingPhrases.yes,
     parameters: [addressParameter],
     inputContextNames: [addressGivenContext.name],
     webhookState: "WEBHOOK_STATE_ENABLED"
